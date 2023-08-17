3f625b71f28ad3bb6821ff4aa1a3474349b00329
Santiago forero <santiago.forero-y@mail.escuelaing.edu.co>
2023-08-17 09:10:44 -0500

    primer trabajo con threads en java

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -5,10 +5,28 @@
  */
 package edu.eci.arsw.threads;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author hcadavid
  */
-public class CountThread {
+public class CountThread extends Thread {
+    private int a;
+    private int b;
+    private LinkedList range = new LinkedList();
+
+    public CountThread(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+    public void run() {
+        for(int i = this.a; i<= this.b;i++){
+            System.out.println(i);
+        }
+    }
+    public LinkedList getRange() {
+        return range;
+    }
     
 }
--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -5,14 +5,30 @@
  */
 package edu.eci.arsw.threads;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author hcadavid
  */
 public class CountThreadsMain {
-    
-    public static void main(String a[]){
-        
+
+    public static void main(String a[]) {
+        CountThread thread1 = new CountThread(0, 99);
+        CountThread thread2 = new CountThread(99, 199);
+        CountThread thread3 = new CountThread(200, 299);
+        thread1.run();
+        thread2.run();
+        thread3.run();
+        LinkedList range1 = thread1.getRange();
+        LinkedList range2 = thread2.getRange();
+        LinkedList range3 = thread3.getRange();
+        //System.out.println("-------------------- VALORES DEL THREAD 1----------------------");
+        //range1.forEach(Integer -> System.out.println(Integer));
+        /*System.out.println("-------------------- VALORES DEL THREAD 2----------------------");
+        range2.forEach(Integer -> System.out.println(Integer));
+        System.out.println("-------------------- VALORES DEL THREAD 3----------------------");
+        range3.forEach(Integer -> System.out.println(Integer));*/
     }
-    
+
 }
